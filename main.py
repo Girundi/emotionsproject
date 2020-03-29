62eeadf9eb48ec841946f5ecc38ff33ecebef2aa
Girundi <43175525+Girundi@users.noreply.github.com>
2020-03-29 21:48:34 +0300

    Update main.py

--- main.py
+++ main.py
@@ -10,10 +10,10 @@ a = Emanalisis(output_mode=2, input_mode=1, channel=cam504, record_video=True, o
 while True:
     now = datetime.datetime.now()
     if 9 <= int(now.strftime("%H")) :
-        # if now.strftime("%m") == "00" or now.strftime("%m") == "30":
-        if platform.system() == 'Windows':
-            filename = now.strftime("%Y-%m-%d_%H-%M")
-        else:
-            filename = now.strftime("%Y-%m-%d_%H:%M")
+        if now.strftime("%m") == "00" or now.strftime("%m") == "30":
+          if platform.system() == 'Windows':
+              filename = now.strftime("%Y-%m-%d_%H-%M")
+          else:
+              filename = now.strftime("%Y-%m-%d_%H:%M")
 
         a.run(filename=filename, fps_factor=25, stop_time=1700)
