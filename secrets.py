03fc422b13a7f6d5dba2e15b9785bf1b1f541ba4
Girundi <43175525+Girundi@users.noreply.github.com>
2020-04-04 17:49:20 +0300

    Update secrets.py

--- secrets.py
+++ secrets.py
@@ -1,5 +1,5 @@
 server_key = "kqhjrfhvbqerbvvr342"
 server_url = 'https://server.url.com/somewhere'
-ip_camera_login = 'admin'
-ip_camera_password = 'admin123'
+ip_camera_login = ''
+ip_camera_password = ''
 credentials_file = 'credentials.json'
