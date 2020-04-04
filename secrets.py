7f2b059a7eca3675209e9ced9aeaba5d96e3094f
Girundi <43175525+Girundi@users.noreply.github.com>
2020-04-04 17:42:47 +0300

    Create secrets.py

---
+++ secrets.py
@@ -0,0 +1,5 @@
+server_key = "kqhjrfhvbqerbvvr342"
+server_url = 'https://server.url.com/somewhere'
+ip_camera_login = 'admin'
+ip_camera_password = 'admin123'
+credentials_file = 'credentials.json'
